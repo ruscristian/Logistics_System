f87f41a3598ea1119c0075e63ae152167d939d1b
Andrei <rusandrei95@gmail.com>
2022-01-11 21:29:47 +0200

    initial 2

--- src/main/java/sci/final_project/logistics_system/shipping/ShippingService.java
+++ src/main/java/sci/final_project/logistics_system/shipping/ShippingService.java
@@ -1,9 +1,19 @@
 package sci.final_project.logistics_system.shipping;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import sci.final_project.logistics_system.GlobalData;
+import sci.final_project.logistics_system.destination.DestinationEntity;
 import sci.final_project.logistics_system.destination.DestinationRepository;
+import sci.final_project.logistics_system.order.OrdersEntity;
 import sci.final_project.logistics_system.order.OrdersRepository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 @Service
@@ -41,18 +51,28 @@ public class ShippingService {
     //     Se va folosi un executor cu maximum 4 threaduri active
     //     si un queue size de 100 de taskuri pentru a gestiona livrarile.
 
-
+    Logger logger = LoggerFactory.getLogger(ShippingService.class);
 
     public void newDayMaker(){
         globalData.setCurrentDate(globalData.getCurrentDate().plusDays(1));
-        //TODO status comanda
+        List<OrdersEntity> ordersForCurrentDate = new ArrayList<>();
+        //TODO get orders for current date
+
+        //map orders to destinations
+        Map<DestinationEntity, List<OrdersEntity>> ordersByDestination = new HashMap<>();
+        //TODO fill map
+
+        for (DestinationEntity destination : ordersByDestination.keySet()) {
+            threadCourier(destination, ordersByDestination.get(destination));
+        }
     }
 
-    public void threadCourier(){
+    @Async(value = "taskExecutor")
+    public void threadCourier(DestinationEntity destination, List<OrdersEntity> orders){
 
     }
 
- 
+
 
 
 }

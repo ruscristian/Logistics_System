05b05c3cbb3de770963be759ff704ee898d37940
Stefan Beldiman <stefanbel23@gmail.com>
2022-01-13 21:43:52 +0200

    AddOrders

--- src/main/java/sci/final_project/logistics_system/order/OrderController.java
+++ src/main/java/sci/final_project/logistics_system/order/OrderController.java
@@ -2,6 +2,7 @@ package sci.final_project.logistics_system.order;
 
 
 import org.springframework.web.bind.annotation.*;
+import sci.final_project.logistics_system.destination.DestinationRepository;
 
 import java.util.List;
 
@@ -12,18 +13,23 @@ public class OrderController {
 
  private final OrdersService ordersService;
  private final OrdersRepository ordersRepository;
+ private final DestinationRepository destinationRepository;
 
-    public OrderController(OrdersService ordersService, OrdersRepository ordersRepository) {
+    public OrderController(OrdersService ordersService, OrdersRepository ordersRepository, DestinationRepository destinationRepository) {
         this.ordersService = ordersService;
         this.ordersRepository = ordersRepository;
+        this.destinationRepository = destinationRepository;
     }
 
 
     @GetMapping("/get")
     public List<OrdersEntity> getOrders(){
 
-//        return ordersService.getAllOrders();
         return ordersRepository.findAll();
     }
 
+    @PostMapping("/add")
+    public OrdersEntity addOrder(@RequestBody OrdersEntity payload, String destinationName) throws IllegalArgumentException {
+        return ordersService.addOrder(payload, destinationName, destinationRepository);
+    }
 }
--- src/main/java/sci/final_project/logistics_system/order/OrdersService.java
+++ src/main/java/sci/final_project/logistics_system/order/OrdersService.java
@@ -1,7 +1,9 @@
 package sci.final_project.logistics_system.order;
 
 import org.springframework.stereotype.Service;
+import sci.final_project.logistics_system.GlobalData;
 import sci.final_project.logistics_system.destination.DestinationEntity;
+import sci.final_project.logistics_system.destination.DestinationRepository;
 
 import java.util.*;
 
@@ -12,9 +14,26 @@ public class OrdersService {
     private final OrdersRepository ordersRepository;
 
 
+
     public OrdersService(OrdersRepository ordersRepository) {
         this.ordersRepository = ordersRepository;
     }
 
+    public OrdersEntity addOrder(OrdersEntity payload, String destinationName, DestinationRepository destination) {
+        OrdersEntity newOrder = payload;
+        newOrder.setStatus(StatusEnum.NEW);
+        newOrder.setLastUpdated("15-12-2021");
+        List<DestinationEntity> destinations = destination.findAll();
+        for (DestinationEntity destination1 : destinations) {
+            if (destination1.getName().equals(destinationName))
+                newOrder.setDestination(destination1);
+        }
+        ordersRepository.save(newOrder);
+        return newOrder;
+    }
 
 }
+
+
+
+

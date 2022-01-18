e27ee99bbd5cf6d64bdcdbeea1402b72ae97edff
Stefan Beldiman <stefanbel23@gmail.com>
2022-01-18 13:14:04 +0200

    Loggers added

--- src/main/java/sci/final_project/logistics_system/order/OrdersService.java
+++ src/main/java/sci/final_project/logistics_system/order/OrdersService.java
@@ -1,12 +1,15 @@
 package sci.final_project.logistics_system.order;
 
 import org.aspectj.weaver.ast.Or;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import sci.final_project.logistics_system.GlobalData;
 import sci.final_project.logistics_system.destination.DestinationEntity;
 import sci.final_project.logistics_system.destination.DestinationRepository;
+import sci.final_project.logistics_system.shipping.ShippingService;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -23,6 +26,7 @@ public class OrdersService {
     private final GlobalData globalData;
     private final DestinationRepository destinationRepository;
 
+    Logger logger = LoggerFactory.getLogger(OrdersService.class);
 
     public OrdersService(OrdersRepository ordersRepository, GlobalData globalData, DestinationRepository destinationRepository, DestinationRepository destinationRepository1) {
         this.ordersRepository = ordersRepository;
@@ -32,6 +36,7 @@ public class OrdersService {
 
     public ResponseEntity<OrdersEntity> addOrder(OrdersEntity payload, DestinationRepository destination) {
         if (compareDates(payload.getDeliveryDate())) {
+            logger.info("Date is older than today.");
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         } else {
             OrdersEntity newOrder = payload;
@@ -43,6 +48,7 @@ public class OrdersService {
                     newOrder.setDestination(destination1);
             }
             ordersRepository.save(newOrder);
+            logger.info("New order added: " + newOrder);
             return new ResponseEntity<>(newOrder, HttpStatus.CREATED);
         }
     }
@@ -62,16 +68,19 @@ public class OrdersService {
 
     public ResponseEntity<OrdersEntity> cancelOrder(OrdersEntity payload) {
         if (payload.getId() == null) {
+            logger.info("Provided ID does not exist.");
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         if (ordersRepository.findById(payload.getId()).isPresent()) {
             OrdersEntity orderToCancel = ordersRepository.getById(payload.getId());
             if (orderToCancel.getStatus().equals(StatusEnum.DELIVERED)) {
+                logger.info("Order already delivered.");
                 return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
             } else {
                 orderToCancel.setStatus(StatusEnum.CANCELLED);
                 orderToCancel.setLastUpdated(computeLastUpdated());
                 ordersRepository.save(orderToCancel);
+                logger.info("Order cancelled: " + orderToCancel) ;
                 return new ResponseEntity<>(orderToCancel, HttpStatus.ACCEPTED);
             }
         } else {
@@ -82,14 +91,15 @@ public class OrdersService {
     public List<OrdersEntity> findOrdersByCriteria(String destination, String date, GlobalData globalData) {
         List<DestinationEntity> destinations = destinationRepository.findAll();
         List<OrdersEntity> resultedList = ordersRepository.findAll();
+        boolean orderFound = false;
 
         if (destination != null && date != null) {
             for (DestinationEntity destination1 : destinations) {
                 if (destination1.getName().equals(destination)) {
-                  return  resultedList = ordersRepository.findByDestinationIdAndDeliveryDate(destination1.getId(), date);
-                }
-                else {
-                    System.out.println("Destination does not exist");
+                    orderFound = true;
+                    logger.info("Orders list for " + destination + " on " + date + " has been generated.");
+                    return resultedList = ordersRepository.findByDestinationIdAndDeliveryDate(destination1.getId(), date);
+
                 }
             }
         }
@@ -98,16 +108,18 @@ public class OrdersService {
             String formattedString = globalData.getCurrentDate().format(formatter);
             for (DestinationEntity destination1 : destinations) {
                 if (destination1.getName().equals(destination)) {
+                    logger.info("Orders list for " + destination + " on current application date has been generated.");
                   return resultedList = ordersRepository.findByDestinationIdAndDeliveryDate(destination1.getId(), formattedString);
-                }
-                else {
-                    System.out.println("Destination does not exist");
                 }
             }
         }
         if (destination == null && date != null) {
+            logger.info("Found orders by delivery date.");
            return resultedList = ordersRepository.findByDeliveryDate(date);
         }
+
+        logger.info("Destination does not exist. Full orders list returned.");
         return resultedList = ordersRepository.findAll();
+
     }
 }
